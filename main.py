fdbe20328434c6df62c63af24cd445b8785a59b3
Alan Tomusiak <atomusiak6@gmail.com>
2019-12-26 13:37:50 -0800

    Organization refined

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from predict_parser import *
 from trends_parser import *
 
 #Set these values only
+directory = "data/"
 candidates = ["Biden","Warren","Sanders","Buttigieg","Klobuchar","Yang","Booker","Steyer","Bloomberg"]
 predictit_data = "PredictItData.csv"
-data = processData(predictit_data, candidates)
+data = processData(directory + predictit_data, candidates)
 #export_csv = data.to_csv(r'processedPredictIt.csv', index = None, header=True)
 ## Export to save time if desired
 print(data)
--- trends_parser.py
+++ trends_parser.py
@@ -2,8 +2,9 @@ from pytrends.dailydata import *
 import pandas as pd
 import numpy
 
-df = pd.DataFrame(get_daily_data("Joe Biden", 2019, 10, 2019,12))
-export_csv = df.to_csv(r'Biden.csv', index = None, header=True)
-print(df.head())
-df.to_csv(index=False)
-input(".")
+def definitely_a_function():
+    df = pd.DataFrame(get_daily_data("Joe Biden", 2019, 10, 2019,12))
+    export_csv = df.to_csv(r'Biden.csv', index = None, header=True)
+    print(df.head())
+    df.to_csv(index=False)
+    input(".")
